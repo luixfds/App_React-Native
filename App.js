39b4b3c0db553ea7dcc09cae777cead5ba6fc116
luixfds <l.fds@yahoo.com>
2021-11-18 09:55:12 -0300

    home quase pronta

--- App.js
+++ App.js
@@ -15,9 +15,9 @@ const MyTheme = {
   dark: false,
   colors: {
     primary: 'rgb(0, 0, 0)',
-    background: '#f2f7f2',
-    card: '#FF0000',
-    text: 'rgb(255, 255, 255)',
+    background: '#ffffff',
+    card: '#ffffff',
+    text: 'rgb(255, 14, 0)',
     border: 'rgb(199, 199, 204)',
     notification: 'rgb(255, 69, 58)', 
   },
@@ -31,9 +31,9 @@ class app extends React.Component {
       <NavigationContainer theme={MyTheme} >
         <Stack.Navigator style={styles.header} initialRouteName="Login">
           <Stack.Screen style={styles.header} name="Login" component={login} options={{headerShown: false, }} />
-          <Stack.Screen style={styles.header} name="Home" component={HomeScreen} options={{headerTitle: () => (<Text style={styles.headerMain}>INICIAR PESQUISA</Text>), headerRight: () => (<Image style={styles.iconImg} source={require('./assets/imgs/senai-logo.png')} />), headerTitleAlign: "start"} }/>
+          <Stack.Screen style={styles.header} name="Home" component={HomeScreen} options={{headerTitle: () => (<Text style={styles.headerMain}>Sistemas</Text>), headerRight: () => (<Image style={styles.iconImg} source={require('./assets/imgs/senai-logo.png')} />), headerTitleAlign: "start"} }/>
           <Stack.Screen name="Details" component={listScreen}  />
-          <Stack.Screen name="Quests" component={Quest} //options={{ title: 'Pesquisa de Satisfação' }}
+          <Stack.Screen name="Quests" component={Quest}
             initialParams={{'Number': 1}}  options={{headerShown: false, }} />
           <Stack.Screen name="Comment" component={comm} options={{headerShown: false, }}  />
         </Stack.Navigator>
@@ -48,16 +48,13 @@ export default app;
 
 
 const styles = StyleSheet.create({
-  header:{
-    backgroundColor: 'black'
-  },
   iconImg:{
-    width: 100,
-    height: 30,
-    marginRight: 20
+    width: 110,
+    height: 40,
+    marginRight: 40
   },
   headerMain:{
     fontSize: 20,
-    color: 'white'
+    color: 'black',
   }
 });
--- components/home.js
+++ components/home.js
@@ -7,19 +7,14 @@ function HomeScreen({ navigation }){
   }, []);
   return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Image style={styles.img2} source={require('../assets/imgs/sesi-senai-fundo2.jpg')} blurRadius={1} />
       <TouchableOpacity
         style= {styles.btnInit}
         onPress={() => navigation.push('Quests')}
       >
-      <Text style= {styles.btnInitText}>Iniciar Pesquisa</Text>
+      <Image style={styles.btnInitImg} source={require('../assets/imgs/satis.jpeg')} blurRadius={1} />
+      <Text style= {styles.btnInitText}>Pesquisa de Satisfação</Text>
       </TouchableOpacity>
 
-       <Image style={styles.iconImg} source={require('../assets/imgs/satisfacao.png')} />
-        <TouchableOpacity style={styles.logoffBtn}  onPress={() => navigation.push('Login')}>
-          <Text style={styles.textLogOff}>Logoff</Text>
-          <Image style={styles.logoff} source={require('../assets/imgs/logoff.png')} blurRadius={1} />
-        </TouchableOpacity>
       {}
     </View>
   );
@@ -28,68 +23,25 @@ function HomeScreen({ navigation }){
 export default HomeScreen
 
 const styles = StyleSheet.create({
-  innerText: {
-    color: 'red'
-  },
-  btnPlus: {
-    color: 'white',
-    fontSize: 20,
-    backgroundColor: '#2b2b2b',
-    width: 40,
-    height: 40,
-    display:'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 20,
-    position: 'absolute',
-    top: '90%',
-    left: '85%'
-  },
   btnInit:{
-    backgroundColor: '#75ffbc',
-    
-    width: '50%',
-    height: '10%',
+    backgroundColor: 'white',
+    width: '60%',
+    height: '40%',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 10,
-    
-  },
-  logoffBtn:{
-    width: 90,
-    
-    height: 40,
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    position:'absolute',
-    backgroundColor: '#2b2b2b',
-    top: 10,
-    right: 10,
-    borderRadius: 20
-  },
-  textLogOff:{
-    color: 'white',
-    fontSize: 18
-  },
-  logoff:{
-    top: -1.5,
-    width: 30,
-    height: 30,
-    resizeMode: 'contain',
-
   },
   btnInitText:{
-    color: "white",
+    color: "black",
     fontSize: 20,
   },
-  iconImg:{
-      top: "35%",
-      height: '10%',
-      width: '100%',
-      resizeMode: 'contain'
-  },
+  btnInitImg:{
+    width: '100%',
+    height: '90%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
   
 });
